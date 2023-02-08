7a78288679c57c59ad02f6646c077d487192b5f6
Qumbre Rizvi <qumberrizvi@ymail.com>
2023-02-08 17:24:27 +0530

    entity pagination package

--- src/modules/candidates/entities/candidate.entity.ts
+++ src/modules/candidates/entities/candidate.entity.ts
@@ -1,1 +1,59 @@
-export class Candidate {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity('candidates')
+export class Candidate {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id: number;
+
+  @Column({ name: 'first_name' })
+  @ApiProperty()
+  firstName: string;
+
+  @Column({ name: 'last_name' })
+  @ApiProperty()
+  lastName: string;
+
+  @Column()
+  @ApiProperty()
+  email: string;
+
+  @Column()
+  @ApiProperty()
+  contact: string;
+
+  @Column()
+  @ApiProperty()
+  gender: number;
+
+  @Column()
+  @ApiProperty()
+  qualification: string;
+
+  @Column()
+  @ApiProperty()
+  dob: number;
+
+  @Column()
+  @ApiProperty()
+  address: string;
+
+  @Column()
+  @ApiProperty()
+  resume: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  @ApiProperty()
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  @ApiProperty()
+  updatedAt: Date;
+}
