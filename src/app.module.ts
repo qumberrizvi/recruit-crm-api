7650fe3860be64dc9a51e6e12e2c13d2120141d0
Qumbre Rizvi <qumberrizvi@ymail.com>
2023-02-08 13:43:01 +0530

    initial resource candidates and readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { CandidatesModule } from './candidates/candidates.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { ConfigModule } from '@nestjs/config';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
     }),
+    CandidatesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
